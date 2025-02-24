43f3ce03b46c26a0f716c674985cb43acda72282
martinnyo <martin.veres@student.uni-miskolc.hu>
2025-02-24 14:11:11 +0100

    init commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!,szia");
+        System.out.println("Hello, World!,szi0,hahaa");
     }
 }
